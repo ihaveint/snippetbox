ab658c290e1fae0f8ab32049cb1ebd6cf8e23c39
ihaveint <ihaveint@gmail.com>
2020-03-16 03:21:30 +0330

    Resolve bug in routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,8 +5,8 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("snippet", app.showSnippet)
-	mux.HandleFunc("snippet/create", app.createSnippet)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("static/", http.StripPrefix("/static", fileServer))
