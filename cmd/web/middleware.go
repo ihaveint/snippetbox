3b406f2d680eb6e670223922ec899bae43037f90
ihaveint <ihaveint@gmail.com>
2020-03-16 06:11:48 +0330

    Add middleware for panic recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
 
 func (app *application) logRequest(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,5 +10,5 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
