0642ed0fde9501702e4d695cc2c2d3725e55aa6e
ihaveint <ihaveint@gmail.com>
2020-03-16 05:53:08 +0330

    Resolve bug in routes for static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	return secureHeaders(mux)
 }
