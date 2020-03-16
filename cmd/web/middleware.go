b7612c650882d6ddc57d78a6316d46538e11fa8f
ihaveint <ihaveint@gmail.com>
2020-03-16 05:39:03 +0330

    Add security middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -10,5 +10,5 @@ func (app *application) routes() *http.ServeMux {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("static/", http.StripPrefix("/static", fileServer))
-	return mux
+	return secureHeaders(mux)
 }
