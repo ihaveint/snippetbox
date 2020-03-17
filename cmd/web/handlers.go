f98af62aac842c1d304ea20db4434b3ca67974cd
ihaveint <ihaveint@gmail.com>
2020-03-17 06:35:15 +0330

    Add logout functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -155,5 +155,8 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Logout the user...")
+	app.session.Remove(r, "userID")
+
+	app.session.Put(r, "flash", "You've been logged out successfully!")
+	http.Redirect(w, r, "/", 303)
 }
