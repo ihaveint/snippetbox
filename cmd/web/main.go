ab5b27afd75d279766a774d79e5baf00f65b8d73
ihaveint <ihaveint@gmail.com>
2020-03-17 07:00:08 +0330

    Add SameSite strict mode

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -48,6 +48,7 @@ func main() {
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
 	session.Secure = true
+	session.SameSite = http.SameSiteStrictMode
 
 	app := &application{
 		errorLog:      errorLog,
