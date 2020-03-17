6713bebd62ad6250af847f7154ff76f9b8951f72
ihaveint <ihaveint@gmail.com>
2020-03-17 05:06:05 +0330

    Add user signup form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -80,7 +80,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display the user signup form ...")
+	app.render(w, r, "signup.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
