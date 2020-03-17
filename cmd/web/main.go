a308eb0446ddf17912f79869424271868730c423
ihaveint <ihaveint@gmail.com>
2020-03-17 04:30:14 +0330

    Add connection timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -62,10 +62,13 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:      *port,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *port,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Startingg server on %s", *port)
